abc98cfca5ef2f6ce64ee68a78b25ada785f7152
Beng Lon Mok <mok@wenconsulting.com>
2021-09-07 00:51:21 +0800

    move e2e test to correct folder

--- email/test/app.e2e-spec.ts
+++ email/test/app.e2e-spec.ts
@@ -1,24 +1,52 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { ClientProxy, ClientsModule, Transport } from '@nestjs/microservices';
 
-describe('AppController (e2e)', () => {
+describe('Email Service e2e test', () => {
   let app: INestApplication;
+  let client: ClientProxy;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        ClientsModule.register([
+          { name: 'EMAIL_SERVICE', transport: Transport.TCP },
+        ]),
+      ]
+    })
+    .compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.connectMicroservice({
+      transport: Transport.TCP,
+    });
+
+    await app.startAllMicroservices();
     await app.init();
+
+    client = app.get('EMAIL_SERVICE');
+    await client.connect();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('should able to send email', done => {
+    const pattern = 'mail_send';
+    const payload = {
+      from: 'from@test.com',
+      to: 'to@test.com',
+      subject: 'This is subject',
+      html: 'content'
+    };
+
+    client.send(pattern, payload).subscribe(response => {
+      expect(response).toEqual(expect.objectContaining({ status: 'success' }));
+      done();
+    })
+  });
+
+  afterAll(async () => {
+    await app.close();
+    client.close();
   });
 });
